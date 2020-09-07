80fd8f8f9c3b8bc4f0d4170aa09e9de56840a077
stephen yi <stephenyi@stephens-MacBook-Pro.local>
2020-09-07 04:17:24 -0400

    working

--- models/flight.js
+++ models/flight.js
@@ -19,7 +19,8 @@ let destinationSchema = new Schema(
 let flightSchema = new Schema({
     airline: { 
         type: String, 
-        enum: ['American', 'Southwest', 'United'] },
+        enum: ['American', 'Southwest', 'United'] 
+    },
     airport: {
         type: String, 
         enum: ['ATL', 'AUS', 'DFW', 'DEN', 'LAX', 'SAN']
@@ -30,12 +31,12 @@ let flightSchema = new Schema({
     departs: {
         type: Date,
         default: function () {
-          return new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+          return new Date(new Date().setFullYear(new Date().getFullYear() + 1));  
         },
-      },
     },
-    {
+    destinations: [destinationSchema],
+  }, {
       timestamps: true,
-    });
+    })
 
 module.exports = mongoose.model('Flight', flightSchema);
